8d6ff0d99b41d4ec72040551da9c547a53b3852b
LukasMikalauskas1 <150537918+LukasMikalauskas1@users.noreply.github.com>
2023-11-12 22:51:08 +0200

    Add files via upload

---
+++ source/StoreTrack/Data/StoreDbContext.cs
@@ -0,0 +1,37 @@
+﻿using StoreTrack.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+//using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using StoreTrack.Auth.Model;
+
+namespace StoreTrack.Data
+{
+	public class StoreDbContext : IdentityDbContext<StoreRestUser>
+	{
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Item>().ToTable("Item");
+			modelBuilder.Entity<Owner>().ToTable("Owner");
+			modelBuilder.Entity<Shop>().ToTable("Shop");
+			// Other configurations
+		}
+		private readonly IConfiguration _configuration;
+		public DbSet<Owner> Owners { get; set; }
+		public DbSet<Shop> Shops { get; set; }
+		public DbSet<Item> Items { get; set; }
+
+		public StoreDbContext(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//"PostgreSQL"
+			optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgreSQL"));
+			//optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+		}
+	}
+}
